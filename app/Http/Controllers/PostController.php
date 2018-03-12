42946a0130d3ff2072b311aa61b5afca41ca495a
Sabbir <sabbir.ahmad111@gmail.com>
2018-03-12 13:26:58 +0600

    Post-validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,9 +14,12 @@ class PostController extends Controller
      * @return \Illuminate\Http\Response
      */
 
+
+
     public function __construct(){
         
          $this->middleware('auth');
+         
     }
     public function index()
     {
@@ -43,6 +46,10 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
+        $this->validate($request, array(
+                'title'         => 'required|max:255',
+                'body'          => 'required'
+            ));
 
         $post = new Post();
 
